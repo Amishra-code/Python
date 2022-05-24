a41269b5715acb4748e1831f324f1985ee39f207
Anjali Mishra <85864787+Amishra-code@users.noreply.github.com>
2022-05-25 02:00:26 +0530

    Add files via upload

---
+++ Code/04. set.py
@@ -0,0 +1,7 @@
+#set : doesn't sort data
+#      uses a concept of '#' which improves the performance as fast as possible
+
+s={22,25,14,21,5}
+print(s)
+
+# print(s[2])        set doesn't support indexing
