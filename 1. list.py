0246dc6996e62f30ea3cc28181a315ec83d2d838
Anjali Mishra <85864787+Amishra-code@users.noreply.github.com>
2022-05-25 01:56:49 +0530

    Add files via upload

---
+++ 1. list.py
@@ -0,0 +1,12 @@
+#list
+name='youtube'
+print(name)
+print(name + ' rocks')
+print(name[-7])
+print(name[1:4])
+print(name[:4])
+print(name[1:10])
+print(len(name))
+
+print('a:\docs\new')         #(\n means new line)
+print(r'c:\docs\new')        # r here removes the special meaning
