3a18e48d26dfa39a7209861f249588954d1f7922
Anjali Mishra <85864787+Amishra-code@users.noreply.github.com>
2022-05-26 01:18:20 +0530

    Add files via upload

---
+++ Code/07.1 base conversions.py
@@ -0,0 +1,11 @@
+# decimal ------>> binary
+print(bin(25))                               # 0b11001
+print(0b0101)                                # 5
+
+# decimal ------>> octal
+print(oct(25))                               # 0o31
+
+# decimal ------>> hexadecimal
+print(hex(25))                               # 0x19
+print(hex(10))                               # 0xa
+print(0xf)                                   # 15
