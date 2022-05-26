9a504f8a04cde965771913902e229afa5484b91a
Anjali Mishra <85864787+Amishra-code@users.noreply.github.com>
2022-05-27 01:35:12 +0530

    Add files via upload

---
+++ Code/15. if else.py
@@ -0,0 +1,22 @@
+# example 1 :
+x=8
+r=x%2
+if r==0:
+    print("even")
+else:
+    print("odd")
+print("bye")
+
+
+# example 2 :
+x=1
+if (x==1):
+    print("one")
+elif(x==2):
+    print("two")
+elif(x==3):
+    print("three")
+elif(x==4):
+    print("four")
+# expression or condition which has been checked by if or elif inside(), these () are called suite.
+
