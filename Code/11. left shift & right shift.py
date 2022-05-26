4b2b912f541b9104f8d1ead5cb1192ad2d350b20
Anjali Mishra <85864787+Amishra-code@users.noreply.github.com>
2022-05-27 01:33:50 +0530

    Add files via upload

---
+++ Code/11. left shift & right shift.py
@@ -0,0 +1,8 @@
+# using left shift                           
+print(10<<2)                                 # left shift by two bits
+# 10(0b1010.0000) << 40(0b101000.00)         # 00 moves to the integer side
+
+
+# using left shift  
+print(10>>1)                                 # right shift by one bit
+# 10(0b1010.0000) >> 5(0b101.00000)          # 0 moves to the decimal side
