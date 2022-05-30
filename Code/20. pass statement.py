b60609349425d839daf426173176057f2cd88dbf
Anjali Mishra <85864787+Amishra-code@users.noreply.github.com>
2022-05-30 19:53:20 +0530

    Add files via upload

---
+++ Code/20. pass statement.py
@@ -0,0 +1,15 @@
+for i in range(5):
+
+    if i==3:
+        continue
+    print("hello",i)
+
+
+    if i==4:
+        pass
+
+
+ # pass is used when we do not know what to do with that statement or line or function. 
+ # so we just skip it
+ 
+    
