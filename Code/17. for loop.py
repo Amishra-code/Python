2fb9782240d0feca91c721c459381b165109db40
Anjali Mishra <85864787+Amishra-code@users.noreply.github.com>
2022-05-30 19:51:39 +0530

    Add files via upload

---
+++ Code/17. for loop.py
@@ -0,0 +1,25 @@
+# Example 1 :
+x=['Anjali',65,2.5]
+for i in x:                   # i in x means i takes the value of x
+    print(i)
+
+# Example 2 :
+print()
+for i in range(10):          # range is 0-9
+    print(i)
+
+# Example 3 :
+print()
+for i in range (11,21,5):    # ( start , end , increament/decreament )
+    print(i)
+
+# Example 4 :
+print()
+for i in range (20,10,-1):
+    print(i)
+
+# Example 5 :
+print()
+for i in range(1,21):
+    if i%5!=0:
+        print(i)
