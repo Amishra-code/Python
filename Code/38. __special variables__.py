091e4d74b3cdf03e3c914a925c7d03b0abab973a
Anjali Mishra <85864787+Amishra-code@users.noreply.github.com>
2022-06-10 01:17:31 +0530

    Add files via upload

---
+++ Code/38. __special variables__.py
@@ -0,0 +1,3 @@
+import CalcSpecial                # prints HelloCalcSpecial (Hello + Module name)
+
+print("__Special variable__  says : " + __name__)         # prints __Special variable__  says : __main__
